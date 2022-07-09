19e7ee0e830c5a888dcd20847505acea3a1458e5
Rip Ryness <rip@ryness.net>
2022-07-08 22:50:46 -0600

    feat: improved mint flow

--- app/routes/mint.jsx
+++ app/routes/mint.jsx
@@ -46,12 +46,13 @@ export default function Mint() {
 export function MintUi({ childRef }) {
   const [nfts, setNfts] = useState([]);
   const [walletAddress, setWalletAddress] = useState("");
+  const [showConfirmDialog, setShowConfirmDialog] = useState(false);
 
   const selectAll = () => {
     setNfts(
       nfts.map((v) => ({
         ...v,
-        selected: !v.soul && !v.pending,
+        selected: !v.soul?.name && !v.pending,
       }))
     );
   };
@@ -75,61 +76,76 @@ export function MintUi({ childRef }) {
     getNfts();
   }, []);
   return (
-    <Section
-      childRef={childRef}
-      className="MintUi fixed top-0 left-0 w-full h-full py-2 pb-[5vh] pt-[15vh]"
-    >
-      <Cont className="h-full md:w-[90vw] bg-black text-white rounded-xl md:px-12 px-4 pb-12 text-center flex flex-col ">
-        {/* Body */}
-        <div className="flex flex-col justify-center items-center">
-          <div className="max-w-[55ch]">
-            <h2 className="font-display text-red md:text-5xl text-3xl mb-4">
-              Select the VOX you want to mint a Soul for.
-            </h2>
+    <>
+      <Section
+        childRef={childRef}
+        className="MintUi fixed top-0 left-0 w-full h-full py-2 pb-[5vh] pt-[15vh]"
+      >
+        <Cont className="h-full md:w-[90vw] bg-black text-white rounded-xl md:px-12 px-4 pb-12 text-center flex flex-col ">
+          {/* Body */}
+          <div className="flex flex-col justify-center items-center">
+            <div className="max-w-[55ch]">
+              <h2 className="font-display text-red md:text-5xl text-3xl mb-4">
+                Select the VOX you want to mint a Soul for.
+              </h2>
+            </div>
           </div>
-        </div>
-        {/* DYNAMIC */}
-        <div className="flex flex-col justify-between grow">
-          <div className="flex justify-end border-b mt-2">
-            <button
-              className="text-xs uppercase p-4"
-              onClick={() => selectAll()}
-            >
-              Select All
-            </button>
-          </div>
+          {/* DYNAMIC */}
+          <div className="flex flex-col justify-between grow">
+            <div className="flex justify-end border-b mt-2">
+              <button
+                className="text-xs uppercase p-4"
+                onClick={() => selectAll()}
+              >
+                Select All
+              </button>
+            </div>
 
-          <div className="md:h-[40vh] h-full md:overflow-y-scroll overflow-auto mb-8 p-4 grid md:grid-cols-4 lg:grid-cols-6 gap-8">
-            {nfts.map((it, i) => (
-              <SoulUi
-                key={i}
-                index={i}
-                content={it}
-                onSelectionChange={(selected) => {
-                  it.selected = !it.soul?.name && !it.pending && selected;
-                  setNfts(
-                    nfts.map((v) => ({
-                      ...v,
-                    }))
-                  );
+            <div className="md:h-[40vh] h-full md:overflow-y-scroll overflow-auto mb-8 p-4 grid md:grid-cols-4 lg:grid-cols-6 gap-8">
+              {nfts.map((it, i) => (
+                <SoulUi
+                  key={i}
+                  index={i}
+                  content={it}
+                  onSelectionChange={(selected) => {
+                    it.selected = !it.soul?.name && !it.pending && selected;
+                    setNfts(
+                      nfts.map((v) => ({
+                        ...v,
+                      }))
+                    );
+                  }}
+                />
+              ))}
+            </div>
+
+            <div>
+              <ConfirmMintButton
+                text="Mint Selected"
+                onClick={async () => {
+                  const selected = nfts.filter((n) => n.selected);
+                  await mint(selected, walletAddress);
+                  await setPending(selected, walletAddress);
+                  setShowConfirmDialog(true);
                 }}
               />
-            ))}
+            </div>
           </div>
-
-          <div>
-            <ConfirmMintButton
-              text="Mint Selected"
-              onClick={async () => {
-                const selected = nfts.filter((n) => n.selected);
-                await mint(selected, walletAddress);
-                setPending(selected);
-              }}
-            />
+        </Cont>
+      </Section>
+      {showConfirmDialog ? (<>
+        <div className="h-screen w-screen backdrop-brightness-50 fixed left-0 top-0 grid justify-center place-items-center">
+          <div className="bg-white p-12 uppercase rounded-2xl font-display drop-shadow-md grid justify-center">
+            <h1 className="text-2xl">mint request successful</h1>
+            <button type="button" className="bg-red text-white pt-4 pb-3 md:px-14 mt-8 px-8 sm:px-2 rounded-lg tx-display uppercase font-display md:text-[1.2em] sm:text-[1em] text-[.8em] leading-[.8em]"
+                    onClick={() => setShowConfirmDialog(false)}
+            >
+              ok
+            </button>
           </div>
         </div>
-      </Cont>
-    </Section>
+      </>) : null}
+    </>
   );
 }
 
@@ -152,7 +168,7 @@ function SoulUi({ content, onSelectionChange }) {
           #{content.tokenId}
         </p>
       </div>
-      {content?.soul?.name ? (<div className="absolute top-[4.25rem] left-8 grid grid-flow-col items-center">
+      {content?.soul?.name ? (<div className="absolute top-12 left-8 grid grid-flow-col items-center">
         <img className="h-12 w-12" src={content.soul.image} alt="" />
         <span className="text-black text-[.8em]">{content.soul.name}</span>
       </div> ): null}
@@ -167,7 +183,7 @@ function SoulUi({ content, onSelectionChange }) {
         </p>
         {content.pending ? (
           <div className="absolute top-0 left-0 h-full w-full backdrop-brightness-50">
-            <div className="absolute top-28 left-8 mx-4 bg-white text-black p-8 h-8 uppercase rounded-2xl uppercase font-display drop-shadow-md grid place-content-center">
+            <div className="absolute top-28 left-10 mx-4 bg-white text-black p-8 h-8 uppercase rounded-2xl font-display drop-shadow-md grid place-content-center">
               pending
             </div>
           </div>
